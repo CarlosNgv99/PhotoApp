7b0889ee9746316a7056acc42d7bb48d9939cd24
carlosngv99 <carlosngv99@hotmail.com>
2020-07-24 22:29:21 -0600

    test3

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -10,7 +10,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname+'/dist/frontend'));
-app.get('/',function(req,res){
+app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
 });
 
